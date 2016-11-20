e40ad1ae74f72811c71656ba6bff4ae1c4983dcb
Chris Luiz <chris@chrisluiz.io>
2016-11-19 21:25:38 -0700

    dynamic routing

--- app.py
+++ app.py
@@ -6,13 +6,29 @@ from flask import Flask
 # create the application object
 app=Flask(__name__)
 
-# use decorator to linl the function to a url
+# error handling
+app.config['DEBUG'] = True
+# use decorator to link the function to a url
 @app.route('/')
 @app.route('/hello')
+@app.route('/test/<search_query>')
 
 #define the view using a function, which returns a string
 def hello_world():
     return 'Hello, World!'
+
+#dynamic route
+@app.route('/test/<search_query>')
+def search(search_query):
+    return search_query
+
+# dyanamic route with explicit status codes
+@app.route('/name/<name>')
+def index(name):
+    if name.lower() == 'michael' :
+        return 'Hello, {}'.format(name), 200
+    else:
+        return 'Not Found', 404
 
 #start the development server using the run method
 if __name__=='__main__':
